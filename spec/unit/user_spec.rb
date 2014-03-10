9fefe0299120c34f15a8708b1b4921f271f01727
Paul Houghton <paul4hough@gmail.com>
2014-03-10 16:23:03 -0600

    unit test passes.

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -6,51 +6,53 @@ require 'spec_helper'
 
 $username = 'paul'
 
-['Fedora','CentOS'].each { |os|
-
-  
-
+$os_family = {
+  'Fedora' => 'redhat',
+  'CentOS' => 'redhat',
+  'Ubuntu' => 'debian',
+}
+['Fedora','CentOS','Ubuntu'].each { |os|
   describe 'master::user', :type => :define do
 
     let(:facts) do {
-        :osfamily  => 'redhat',
-        :operatingsystem => 'Fedora',
+        :osfamily  => $os_family[os],
+        :operatingsystem => os,
     } end 
 
     context "supports operating system: #{os}" do
-      context "defaults" do
-        let(:title) {$username}
-        
-        it "define user #{$username}" do
-          should contain_user($username)
+      context "provides master::user define which" do
+        context "defaults" do
+          let(:title) {$username}
+          it "define user #{$username}" do
+            should contain_user($username)
+          end
+        end
+    
+        context "given rsa key" do
+          let(:title) {$username}
+          
+          let :params do {
+              :rsa  => 'puppet::///extra_files/paul.id_rsa',
+              :home => '/home/paul',
+            } end
+          it { should contain_file('/home/paul/.ssh/id_rsa') }
+          it { should contain_file('/home/paul/.ssh/id_rsa.pub') }
+          it { should contain_file('/home/paul/.ssh/authorized_keys') }
+        end
+        context "libvirt authorization" do
+          let(:facts) do {
+              :osfamily  => 'redhat',
+              :operatingsystem => 'Fedora',
+            } end 
+          let(:title) {$username}
+          let :params do {
+              :libvirt => true,
+            } end
+          it "define libvirt policy for #{$username}" do
+            should contain_policykit__localauthority("paul-libvirt-management")
+          end
         end
       end
-    
-      context "given rsa key" do
-        let(:title) {$username}
-        
-        let :params do {
-            :rsa  => 'puppet::///extra_files/paul.id_rsa',
-            :home => '/home/paul',
-          } end
-        it { should contain_file('/home/paul/.ssh/id_rsa') }
-        it { should contain_file('/home/paul/.ssh/id_rsa.pub') }
-        it { should contain_file('/home/paul/.ssh/authorized_keys') }
-      end
-  # context "libvirt authorization" do
-  #   $username = 'paul'
-  #   let(:facts) do {
-  #       :osfamily  => 'redhat',
-  #       :operatingsystem => 'Fedora',
-  #   } end 
-  #   let(:title) {$username}
-  #   let :params do {
-  #       :libvirt => true,
-  #   } end
-  #   it "define libvirt policy for #{$username}" do
-  #     should contain_policykit_localauthority("paul-libvirt-management")
-  #   end
-  # end
     end
   end
 }
