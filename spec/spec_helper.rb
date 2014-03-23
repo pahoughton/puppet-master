7358918b9a3066fa11a01bbb8cb207f9ab27b94e
Paul Houghton <paul4hough@gmail.com>
2014-03-22 18:32:25 -0600

    fail_fast always

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,10 @@ require 'rspec-puppet'
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
-  c.hiera_config = 'spec/fixtures/hiera.yaml'
+  c.module_path   = File.join(fixture_path, 'modules')
+  c.manifest_dir  = File.join(fixture_path, 'manifests')
+  c.fail_fast     = true,
+  c.hiera_config  = 'spec/fixtures/hiera.yaml'
   c.default_facts = {
     :kernel         => 'Linux',
   }
