97899782711f6d9a0194c1eeacb70c8d9d2c2375
Paul Houghton <paul4hough@gmail.com>
2014-03-25 04:35:42 -0600

    wip - major rework - unittest pass.

--- manifests/nginxphp/app.pp
+++ manifests/nginxphp/app.pp
@@ -3,29 +3,53 @@
 # Copyright (c) 2014 Paul Houghton <paul4hough@gmail.com>
 #
 define master::nginxphp::app (
-  $appdir,
-  $db_host,
-  $db_name,
-  $db_user,
-  $db_pass,
-  $vhost = 'localhost',
+  $basedir   = undef,
   $db_driver = 'mysql',
+  $db_host   = 'localhost',
+  $db_create = true,
+  $db_name   = undef,
+  $db_user   = undef,
+  $db_pass   = undef,
+  $vhost     = 'localhost',
   ) {
 
-  if $db_name {
-    mysql::db { $db_name :
-      host     => $db_host,
-      user     => $db_user,
-      password => $db_pass,
-      grant    => ['ALL',],
+  $directories = hiera('directories',{ 'www' => '/srv/www' })
+
+  $bdir = $basedir ? {
+    undef   => $directories['www'],
+    default => $basedir,
+  }
+
+  $app = $title
+
+  if $db_name and $db_create {
+    case $db_driver {
+      'mysql' : {
+        mysql::db { $db_name :
+          host     => $db_host,
+          user     => $db_user,
+          password => $db_pass,
+          grant    => ['ALL',],
+        }
+      }
+      'postgresql' : {
+        postgresql::server::db { $db_name :
+          owner    => $db_user,
+          user     => $db_user,
+          password => $db_pass,
+        }
+      }
+      default : {
+        fail("unsupported backend '${db_driver}'")
+      }
     }
   }
 
   nginx::resource::location { "${title}_php":
     ensure          => 'present',
     vhost           => $vhost,
-    location        => "~ /${appdir}/(.*\.php)\$",
-    www_root        => $master::nginxphp::basedir,
+    location        => "~ /${app}/(.*\.php)\$",
+    www_root        => $bdir,
     index_files     => ['index.php',],
     proxy           => undef,
     fastcgi         => '127.0.0.1:9000',
@@ -33,8 +57,8 @@ define master::nginxphp::app (
   nginx::resource::location { "${title}_static":
     ensure          => 'present',
     vhost           => $vhost,
-    location        => "~ /${appdir}/.*",
-    www_root        => $master::nginxphp::basedir,
+    location        => "~ /${app}/.*",
+    www_root        => $bdir,
     index_files     => ['index.php',],
     proxy           => undef,
   }
