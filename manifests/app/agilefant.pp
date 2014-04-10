cf2eb6e475130366c54af9f8f7e83ed6501cfab2
Paul Houghton <paul4hough@gmail.com>
2014-04-10 12:48:01 -0600

    major rework to support class arguments and hiera values with defaults.

--- manifests/app/agilefant.pp
+++ manifests/app/agilefant.pp
@@ -4,74 +4,88 @@
 #
 # Fixme - http://java.sun.com/jsp/jstl/fmt cannot be resolved
 class master::app::agilefant (
-  $vhost     = 'localhost',
-  $tport     = undef,
-  $app       = 'agilefant',
-  $db_host   = undef,
-  $mydb_name = 'agilefant',
-  $tomcatdir = '/srv/webapps',
+  $vhost      = 'localhost',
+  $tomcatport = undef,
+  $app        = 'agilefant',
+  $warurl     = undef # wget ${uris[app]}/agilefant-3.4/agilefant.war
+  $db_create  = true,
+  $db_host    = undef, # hiera databases['$app']['host']
+  $db_name    = undef,
+  $db_user    = undef,
+  $db_pass    = undef,
+  $tomcatdir  = '/srv/webapps',
+  $tomcatuser = 'tomcat',
+  $tomcatgrp  = 'tomcat',
   ) {
 
-  $passwords = hiera('passwords',{})
-  $servers   = hiera('servers')
-  $ports     = hiera('ports',{ 'tomcat' => '8080' } )
-  $uris      = hiera('uris')
+  $dhost = $db_host ? { undef => 'localhost', default => $db_host }
+  $dname = $db_name ? { undef => $app, default => $db_name }
+  $duser = $db_user ? { undef => $app, default => $db_user }
+  $dpass = $db_pass ? { undef => $app, default => $db_pass }
+  $tport = $tomcatport ? { undef => '8080', default => $tomcatport }
+  $tuser = $tomcatuser ? { undef => 'tomcat', default => $tomcatuser }
+  $tgrp = $tomcatgrp ? { undef => 'tomcat', default => $tomcatgrp }
+  $tdir = $tomcatdir ? { undef => '/srv/webapps', default => $tomcatdir }
 
-  # fixme - tomcatbase some how?
+
+  $databases   = hiera('databases',{
+    $app => { 'host' => $dhost,
+              'name' => $dname,
+              'user' => $duser,
+              'pass' => $dpass, },})
+  }
+  $directories = hiera('directories',{ 'tomcat', $tdir })
+  $groups      = hiera('groups',{ 'tomcat' => $tgrp })
+  $ports       = hiera('ports',{ 'tomcat' => $tport })
+  #$servers    = hiera('servers')
+  $uris        = hiera('uris',{ 'app' => undef } )
+  $users       = hiera('users',{ 'tomcat' => $tuser })
+
+  $url = $warurl ? {
+    undef   => "${uris[app]}/agilefant-3.4/agilefant.war",
+    default => $warurl,
+  }
+
   Exec {
-    user    => 'tomcat', # $master::app::tomcatbase::user,
-    group   => 'tomcat', # $master::app::tomcatbase::group,
+    user    => $users['tomcat'],
+    group   => $groups['tomcat'],
     require => Class['master::app::tomcatbase'],
-  }
-  $port = $tport ? {
-    undef   => $ports['tomcat'],
-    default => $tport,
-  }
-  if ! $port {
-    fail('need tport (tomcat port) value, hiera ports:tomcat not defined')
   }
   class { 'master::app::tomcatbase' :
     vhost     => $vhost,
-    tport     => $port,
+    tport     => $ports['tomcat'],
     app       => $app,
-    tomcatdir => $tomcatdir,
+    tomcatdir => $directories['tomcat'],
   }
 
-  $mydb_host = $db_host ? {
-    undef   => $servers['mysql'],
-    default => $db_host,
-  }
-
-  $mydb_user = 'agilefant'
-  $mydb_pass = $passwords['mysql-agilefant']
-  if $mydb_host == $::hostname or $mydb_host == 'localhost' {
-    mysql::db { $mydb_name :
-      host     => $mydb_host,
-      user     => $mydb_user,
-      password => $mydb_pass,
+  if $db_create {
+    mysql::db { $databases[$app]['name'],
+      host     => '%',
+      user     => $databases[$app]['user'],
+      password => $databases[$app]['pass'],
       grant    => ['ALL',],
     }
   }
   exec { 'fetch-agilefant' :
-    cwd      => $tomcatdir,
-    command  => "wget ${uris[app]}/agilefant-3.4/agilefant.war",
-    creates  => "${tomcatdir}/agilefant.war",
+    cwd      => $directories['tomcat'],
+    command  => "wget ${url}",
+    creates  => "${directories['tomcat']}/agilefant.war",
     notify   => Service['tomcat'],
-    require  => File[$tomcatdir],
+    require  => File[$directories['tomcat']],
   }
   ->
   exec { 'extract-agilefant' :
     cwd      => $tomcatdir,
     command  => 'unar agilefant.war',
-    creates  => "${tomcatdir}/agilefant",
+    creates  => "${directories[tomcat]}/agilefant",
     notify   => Service['tomcat'],
-    require  => File[$tomcatdir],
+    require  => File[$directories['tomcat']]
   }
   ->
   file { "${tomcatdir}/agilefant/WEB-INF/agilefant.conf" :
     ensure  => 'file',
-    owner   => 'tomcat',
-    group   => 'tomcat',
+    owner   => $users['tomcat'],
+    group   => $groups['tomcat'],
     content => template('master/app/agilefant.conf.erb'),
     notify  => Service['tomcat'],
   }
