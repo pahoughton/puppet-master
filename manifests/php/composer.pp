2b33b62d14326761caeb187e6618120b1cf9751a
Paul Houghton <paul4hough@gmail.com>
2014-04-22 15:19:23 -0600

    php composer

---
+++ manifests/php/composer.pp
@@ -0,0 +1,17 @@
+# composer.pp - 2014-04-11 14:00
+#
+# Copyright (c) 2014 Paul Houghton <paul4hough@gmail.com>
+#
+class master::php::composer {
+
+  exec { 'curl -sS https://getcomposer.org/installer | php' :
+    cwd     => $::root_home,
+    creates => "${::root_home}/composer.phar",
+  }
+  ->
+  file { '/usr/local/bin/composer' :
+    ensure   => 'file',
+    mode     => '0755',
+    source   => "${::root_home}/composer.phar",
+  }
+}
