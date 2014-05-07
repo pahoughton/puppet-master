da03a85e7203eb77469bc1b25e8208735070e6c9
Paul Houghton <paul4hough@gmail.com>
2014-05-07 04:05:57 -0600

    ubuntu port.

--- manifests/app/lclgitlab.pp
+++ manifests/app/lclgitlab.pp
@@ -4,7 +4,9 @@
 #
 class master::app::lclgitlab (
   $git_create_user = true,
+  $vhost           = undef,
   ) {
+
   # fixme args for all values
   $databases = hiera('databases', {
     'gitlab' => {
@@ -17,15 +19,26 @@ class master::app::lclgitlab (
   $directories = hiera('directories',{'gitlab' => '/var/lib/gitlab'})
   $email       = hiera('emails', { 'gitlab' => "gitlab@${::hostname}" })
   $groups      = hiera('groups',{'gitlab' => 'git' })
-  $servers     = hiera('servers',{ 'pgsql' => 'localhost' } )
+  $servers     = hiera('servers',{'pgsql' => 'localhost',
+                                  'www'   => undef } )
   $users       = hiera('users', { 'git' => 'git', 'gitlab' => 'gitlab', })
 
-  ensure_packages(['redis',],)
+  $redispkgs = $::osfamily ? {
+    'Debian'  => ['redis-server',],
+    default   => ['redis',],
+  }
+  $redissvc = $::osfamily ? {
+    'Debian'  => 'redis-server',
+    default   => 'redis',
+  }
 
-  service { 'redis' :
+
+  ensure_packages($redispkgs)
+
+  service { $redissvc :
     ensure  => 'running',
     enable  => true,
-    require => Package['redis'],
+    require => Package[$redispkgs],
   }
 
   if  $databases['gitlab']['host'] == 'localhost' or
@@ -45,18 +58,41 @@ class master::app::lclgitlab (
                                       $databases['gitlab']['pass']),
     }
   }
-
+  $tcpport = '18212' # todo - hardcode gitlab defaults to 80
+  # fixme - gitlab service not refreshing on config file change
   class { 'gitlab' :
-    git_create_user => $git_create_user,
-    git_home        => $directories['gitlab'],
-    git_email       => $email['gitlab'],
-    git_comment     => 'gitolite and gitlab user',
-    gitlab_dbtype   => 'pgsql',
-    gitlab_dbhost   => $databases['gitlab']['host'],
-    gitlab_dbname   => $databases['gitlab']['name'],
-    gitlab_dbuser   => $databases['gitlab']['user'],
-    gitlab_dbpwd    => $databases['gitlab']['pass'],
-    gitlab_repodir  => "${directories[gitlab]}/repositories",
-    require         => Service['redis'],
+    git_create_user          => $git_create_user,
+    git_home                 => $directories['gitlab'],
+    git_email                => $email['gitlab'],
+    git_comment              => 'gitolite and gitlab user',
+    gitlab_dbtype            => 'pgsql',
+    gitlab_dbhost            => $databases['gitlab']['host'],
+    gitlab_dbname            => $databases['gitlab']['name'],
+    gitlab_dbuser            => $databases['gitlab']['user'],
+    gitlab_dbpwd             => $databases['gitlab']['pass'],
+    gitlab_http_port         => $tcpport,
+    gitlab_relative_url_root => '/gitlab',
+    gitlab_repodir           => "${directories[gitlab]}/repositories",
+    require                  => Service[$redissvc],
+  }
+
+  if $vhost {
+    # todo - add unit test
+    file { '/etc/nginx/conf.d/gitlab.conf' :
+      ensure  => 'absent',
+      require => Class['gitlab'],
+    }
+    nginx::resource::upstream { 'gitlab' :
+      ensure  => 'present',
+      members => ["unix:${directories[gitlab]}/gitlab/tmp/sockets/gitlab.socket",],
+    }
+    nginx::resource::location { '/gitlab' :
+      ensure              => 'present',
+      vhost               => $vhost,
+      location            => '/gitlab',
+      proxy               => 'http://gitlab',
+      proxy_read_timeout  => undef,
+      location_cfg_append => { include => '/etc/nginx/conf.d/proxy.conf' },
+    }
   }
 }
