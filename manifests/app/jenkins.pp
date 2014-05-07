43c399ad0e6486f52d64323151a0cc18fe04ce0e
Paul Houghton <paul4hough@gmail.com>
2014-05-07 04:05:47 -0600

    ubuntu port.

--- manifests/app/jenkins.pp
+++ manifests/app/jenkins.pp
@@ -3,14 +3,39 @@
 # Copyright (c) 2014 Paul Houghton <paul4hough@gmail.com>
 #
 class master::app::jenkins (
-  $basedir  = '/srv/jenkins',
-  $user     = 'jenkins',
-  $port     = '7237',
-  $vhost    = undef,
-  $location = '/jenkins'
+  $appdir    = undef,
+  $appuser   = undef,
+  $appgroup  = undef,
+  $appport   = undef,
+  $vhost     = undef,
+  $location  = '/jenkins'
   ) {
+  $directories = hiera('directories',{'jenkins' => '/var/lib/jenkins'})
+  $email       = hiera('emails', { 'gitlab' => "gitlab@${::hostname}" })
+  $groups      = hiera('groups',{'gitlab' => 'git' })
+  $ports       = hiera('ports',{'jenkins' => '8180' })
+  $servers     = hiera('servers',{'pgsql' => 'localhost','www' => undef } )
+  $users       = hiera('users', { 'git' => 'git', 'gitlab' => 'gitlab', })
+
+  $adir = $appdir ? {
+    undef   => $directories['jenkins'],
+    default => $appdir,
+  }
+  $user = $appuser ? {
+    undef   => $users['jenkins'],
+    default => $appuser,
+  }
+  $group = $appgroup ? {
+    undef   => $groups['jenkins'],
+    default => $appgroup,
+  }
+  $port = $appport ? {
+    undef   => $ports['jenkins'],
+    default => $appport,
+  }
   case $::osfamily {
-    'redhat' : {
+    'RedHat' : {
+      $configfn = '/etc/sysconfig/jenkins'
       $keyfn = 'jenkins-ci.org.key'
       file { "/etc/pki/rpm-gpg/${keyfn}" :
         source => "puppet:///modules/master/${keyfn}",
@@ -24,15 +49,24 @@ class master::app::jenkins (
         includepkgs => 'jenkins*',
         gpgkey      => "file:///etc/pki/rpm-gpg/${keyfn}",
       }
-      ->
-      package { 'jenkins' :
-        ensure   => 'installed',
-        require  => Class['java'],
-      }
-      $configfn = '/etc/sysconfig/jenkins'
       # fixme - add this to nginx as needed.
       exec { 'setsebool -P httpd_can_network_connect 1' :
         command => 'setsebool -P httpd_can_network_connect 1',
+      }
+    }
+    'Debian' : {
+      $configfn = '/etc/default/jenkins'
+
+      # apt::key { 'jenkins':
+      #   key        => 'D50582E6',
+      #   key_source => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
+      # }
+      # -> # todo - this should not be needed
+      apt::source { 'jenkins' :
+        location    => 'http://pkg.jenkins-ci.org/debian',
+        release     => '',
+        repos       => 'binary/',
+        include_src => false,
       }
     }
     default : {
@@ -40,34 +74,37 @@ class master::app::jenkins (
     }
   }
 
+  package { 'jenkins' :
+    ensure   => 'installed',
+    require  => Class['java'],
+  }
   file { $configfn :
     ensure  => 'file',
     mode    => '0644',
-    content => template('master/jenkins/config.erb'),
+    content => template("master/app/jenkins.${::osfamily}.erb"),
     notify  => Service['jenkins'],
     require => Package['jenkins'],
   }
-  file { $basedir :
+  file { $adir :
     ensure  => 'directory',
-    owner   => 'jenkins',
-    group   => 'jenkins',
+    owner   => $user,
+    group   => $group,
     mode    => '0775',
     require => Package['jenkins'],
   }
   service { 'jenkins' :
     ensure  => 'running',
     enable  => true,
-    require => [File['/etc/sysconfig/jenkins'],File[$basedir],],
+    require => [File[$configfn],File[$adir],],
   }
   if $vhost {
-    nginx::resource::location { 'jenkins_proxy' :
+    nginx::resource::location { 'jenkins' :
       ensure              => 'present',
       vhost               => $vhost,
-      location            => "~ ^${location}/",
+      location            => $location,
       proxy               => "http://${vhost}:${port}",
       proxy_read_timeout  => undef,
       location_cfg_append => { include => '/etc/nginx/conf.d/proxy.conf' },
     }
   }
-
 }
